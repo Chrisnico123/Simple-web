9f62c510bfd9a15184432f4e8c1dffbc5c9f4a9b
Chrisnico123 <hutapeniko@gmail.com>
2022-06-01 12:30:00 +0700

    Add button js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 function detail() {
   document.getElementById("btn1").addEventListener("click", () => {
-    document.getElementById("detail").innerHTML = "Hellow";
+    detail = document.getElementById("detail");
+    if (detail.hidden == true) {
+      detail.hidden = false;
+      document.getElementById("btn1").innerHTML = "Close";
+    } else {
+      detail.hidden = true;
+      document.getElementById("btn1").innerHTML = "Selengkapnya";
+    }
+  });
+}
+function detail1() {
+  document.getElementById("btn2").addEventListener("click", () => {
+    detail = document.getElementById("detail1");
+    if (detail.hidden == true) {
+      detail.hidden = false;
+      document.getElementById("btn2").innerHTML = "Close";
+    } else {
+      detail.hidden = true;
+      document.getElementById("btn2").innerHTML = "Selengkapnya";
+    }
   });
 }
